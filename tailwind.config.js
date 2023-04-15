f1c35674d5a0a043672fc9d1bdb9f0f883642d2b
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-15 23:06:46 +0300

    Include a background image object with gradient-yellowred & mobilehome properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,12 @@ export default {
         "primary-500": "#FF6B66",
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
-      }
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+        "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
+      }),
     },
   },
   plugins: [],
