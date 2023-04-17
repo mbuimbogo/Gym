db12edfe00b299e915e70ef8206616e7c3424175
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-17 21:52:23 +0300

    Add media querries  with a breakpoint & add a toggle button for smaller screens

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,7 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/type";
-import useMediaQuery from "@/hooks/UseMediaQuerry";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -11,8 +11,9 @@ type Props = {
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
   const flexBetween = "flex items-center justify-between";
-  const isAboveMediumScreens = useMediaQuery("min-width: 1060px")
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
 
   return (
     <nav>
@@ -24,37 +25,46 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img src={Logo} alt="logo" />
 
             {/* Right Side */}
-            <div className={`${flexBetween} w-full`}>
-              {/* inner left side */}
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {isAboveMediumScreens ? (
+                <div className={`${flexBetween} w-full`}>
+                {/* inner left side */}
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+  
+                {/* inner right side */}
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <button>Become a member</button>
+                </div>
               </div>
-
-              {/* inner right side */}
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a member</button>
-              </div>
-            </div>
+            ): (
+                <button className="rounded-full bg-yellow-500 p-2"
+                onClick={()=>setIsMenuToggled(!isMenuToggled)}
+                >
+                    <Bars3Icon className="h-6 w-6 text-white"/>
+                </button>
+            )}
+            
           </div>
         </div>
       </div>
