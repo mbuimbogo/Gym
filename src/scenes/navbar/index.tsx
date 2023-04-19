4d64fb6e1d5bbb81ab65c5be04df9d4ddf7ae972
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 00:07:35 +0300

    Add mobile menu model

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -69,6 +69,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+
+      {/* Mobile menu model */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* Close icon */}
+          <div className="flex justify-end p-12">
+          <button onClick={()=> setIsMenuToggled(!isMenuToggled)}>
+            <XMarkIcon className="w-6 h-6 text-gray-400"/>
+          </button>
+          </div>
+          {/* Menu Items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+        </div>
+
+      )}
     </nav>
   );
 };
