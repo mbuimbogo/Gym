d006101468449e59c8826fc3cd6f66787c9cc437
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-21 00:55:49 +0300

    Create ourclasses component

---
+++ src/scenes/ourclasses/index.tsx
@@ -0,0 +1,13 @@
+import { SelectedPage } from '@/shared/type'
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const OurClasses = ({setSelectedPage}: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default OurClasses
