7dcba211c6e4233d05df4a2fbae50cb5b3498c1a
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 12:11:50 +0300

    Add animation using framer motion

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,8 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SPonsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -22,7 +24,18 @@ const Home = ({ setSelectedPage }: Props) => {
         {/* main header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <div className="md:-mt-20">
+          <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition = {{ duration: 0.5 }}
+            variants= {{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+            }}
+
+            >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:z-[-1] before:-left-20 md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
@@ -33,9 +46,18 @@ const Home = ({ setSelectedPage }: Props) => {
               Studios to get the Body Shapes That you Dream of..Get Your Dream
               Body Now.
             </p>
-          </div>
+          </motion.div>
           {/* Actions */}
-          <div className="mt-8 flex items-center gap-8 ">
+          <motion.div className="mt-8 flex items-center gap-8 "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition = {{ delay: 0.2, duration: 0.5 }}
+          variants= {{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+          }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -46,7 +68,7 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* image */}
