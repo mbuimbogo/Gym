2ec00ce2e40619aa7e03316e08c33336ac82cfed
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-21 00:48:58 +0300

    Change the  benefits child variant opacity to 1 in animation

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -4,8 +4,8 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const childVariant = {
   hidden: { opacity: 0, scale: 0.9 },
-  visible: { opacity: 0, scale: 1 }
-}
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
@@ -16,9 +16,10 @@ type Props = {
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <motion.div 
-    variants={childVariant}
-    className="mt-5 rounded-md border-2 border-gray-100 px-5 md:px-16 py-16 text-center">
+    <motion.div
+      variants={childVariant}
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center md:px-16"
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
