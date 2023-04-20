f7fb8cb41d7e6decb489e0ae18dc1b11216d0de3
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 13:31:42 +0300

    Create benefits component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import Navbar from "@/scenes/navbar"
-import { useEffect, useState } from "react"
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import Benefits from "./scenes/benefits";
 import Home from "./scenes/home";
-import { SelectedPage } from "./shared/type"
+import { SelectedPage } from "./shared/type";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  useEffect(()=> {
-     const handleScroll = () => {
-      if (window.scrollY === 0){
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home)
+        setSelectedPage(SelectedPage.Home);
       }
-      if (window.scrollY !== 0) setIsTopOfPage(false)
-     }
-     window.addEventListener("scroll", handleScroll);
-     return () => window.removeEventListener("scroll", handleScroll);
-  },[])
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="bg-gray-20"> 
-    <Navbar 
-    isTopOfPage={isTopOfPage}
-    selectedPage={selectedPage}
-    setSelectedPage={setSelectedPage}
-    />
-    <Home setSelectedPage={setSelectedPage}/>
+    <div className="bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,13 @@
+import { SelectedPage } from "@/shared/type"
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const Benefits = ({ setSelectedPage}: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Benefits
