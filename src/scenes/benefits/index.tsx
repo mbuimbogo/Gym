01d3f045955a4c108e19440bafb87188eab487b8
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 14:07:04 +0300

    Import icons, get props add animation and add the header reusable component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,13 +1,39 @@
-import { SelectedPage } from "@/shared/type"
+import HText from "@/shared/HText";
+import { SelectedPage } from "@/shared/type";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Benefits = ({ setSelectedPage}: Props) => {
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <div>index</div>
-  )
-}
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        className=""
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
 
-export default Benefits
+        {/* Header */}
+        <div className="md:my-5 md:w-3/5">
+            <HText>MORE THAN JUST A GYM.</HText>
+            <p className="my-5 text-sm">
+                We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide thorough care into each and every member.
+            </p>
+        </div>
+
+        {/* Benefits */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
