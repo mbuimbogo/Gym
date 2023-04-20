e6f375144a46f6d77ebd87cdafb2020053f18fab
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 12:15:45 +0300

    Add onViewportEnter to home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -9,7 +9,6 @@ import SponsorFortune from "@/assets/SPonsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { motion } from "framer-motion";
 
-
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -20,24 +19,26 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10  md:h-full md:pb-0">
       {/* Image and main header */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* main header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <motion.div 
+          <motion.div
             className="md:-mt-20"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition = {{ duration: 0.5 }}
-            variants= {{
-                hidden: { opacity: 0, x: -50 },
-                visible: { opacity: 1, x: 0 },
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
             }}
-
-            >
+          >
             <div className="relative">
-              <div className="before:absolute before:-top-20 before:z-[-1] before:-left-20 md:before:content-evolvetext">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
@@ -48,15 +49,16 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
           {/* Actions */}
-          <motion.div className="mt-8 flex items-center gap-8 "
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition = {{ delay: 0.2, duration: 0.5 }}
-          variants= {{
+          <motion.div
+            className="mt-8 flex items-center gap-8 "
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
-          }}
+            }}
           >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
@@ -73,23 +75,22 @@ const Home = ({ setSelectedPage }: Props) => {
 
         {/* image */}
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
-            <img alt="home-pageGraphic" src={HomePageGraphic}/>
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
 
-     {/* Sponsors */}
-     {isAboveMediumScreens && (
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
-            <div className="mx-auto w-5/6">
-                <div className="flex w-3/5 items-center justify-between gap-8">
-                    <img src={SponsorRedBull} alt="redbull-sponsor" />
-                    <img src={SponsorForbes} alt="forbes-sponsor" />
-                    <img src={SponsorFortune} alt="fortune-sponsor" />
-
-                </div>
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
             </div>
+          </div>
         </div>
-     )}
+      )}
     </section>
   );
 };
