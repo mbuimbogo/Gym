8b485878f64409fe08a99a06251e908e4db0842e
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-20 14:25:46 +0300

    A dd an array of objects of benefits and map them

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,11 +1,32 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/type";
+import { BenefitType, SelectedPage } from "@/shared/type";
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "You are here to love your body outcome.We provide all types of exercises and all equipemnts are available",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of the Diverse Classes",
+    description:
+      "You are here to love your body outcome.We provide all types of exercises and all equipemnts are available",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "You are here to love your body outcome.We provide all types of exercises and all equipemnts are available",
+  },
+];
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -18,18 +39,27 @@ const Benefits = ({ setSelectedPage }: Props) => {
         className=""
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-
         {/* Header */}
         <div className="md:my-5 md:w-3/5">
-            <HText>MORE THAN JUST A GYM.</HText>
-            <p className="my-5 text-sm">
-                We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide thorough care into each and every member.
-            </p>
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide
+            thorough care into each and every member.
+          </p>
         </div>
 
         {/* Benefits */}
-        <div className="md:flex items-center justify-between gap-8 mt-5">
-
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit 
+            key={benefit.title} 
+            icon={benefit.icon}
+            title={benefit.title}
+            description={benefit.description}
+            setSelectedPage={setSelectedPage}
+            />
+          ))}
         </div>
       </motion.div>
     </section>
